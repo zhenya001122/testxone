2ccb5175b3a6cf3c556858220ecafdae49e19a25
zhenya <sluckltu@gmail.com>
2024-11-16 15:05:13 +0300

    add models CustomUser Collection Linc

--- testxone/settings.py
+++ testxone/settings.py
@@ -119,10 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.CustomUser'
+# AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,8 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    pass
+class CustomUser(models.Model):
+    email = models.EmailField()
+    psw = models.CharField(max_length=30)
 
     def __str__(self):
         return f'{self.email}'  # Строковое представление объекта
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=20, unique=True, verbose_name='коллекция')
+    description = models.CharField(max_length=250, blank=True, verbose_name='Краткое описание')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    user = models.ForeignKey(CustomUser,
+        on_delete=models.CASCADE,
+        related_name="collections",
+        blank=True,
+        null=True,)
+
+
+class Linc(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    description = models.CharField(max_length=250, verbose_name='Краткое описание')
+    url = models.URLField(unique=True, verbose_name='URL')
+    img = models.ImageField(upload_to='img/%Y/%m/%d', default=None,
+                              blank=True, null=True, verbose_name='Изображение')
+    type = models.ManyToManyField(Collection)
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    user = models.ForeignKey(CustomUser,
+         on_delete=models.CASCADE,
+         related_name="URL",
+         blank=True,
+         null=True, )
+
